64b825f53ae6ae85340308608836c46434fcb152
Nassim AIT ALI BRAHAM <n.ait-ali-braham@tum.de>
2025-09-14 18:34:28 +0200

    Update registry.py

--- src/backbones/registry.py
+++ src/backbones/registry.py
@@ -1,5 +1,7 @@
 from .spec_resnet import SpecResNet50, ModifiedResNet50
+from .vit import ViTSmall, ViTTiny
 from .spec_vit import (
+    SpecViTTiny,
     SpecViTSmall,
     SpecViTBase,
     SpecViTLarge,
@@ -10,10 +12,13 @@ from .spec_vit import (
 
 BACKBONE_REGISTRY = {
     "spec_resnet50": SpecResNet50,
-    "spec_vit_small": SpecViTSmall,
+    "spec_vit_tiny": SpecViTTiny,
+    "spec_vit_small": SpecViTSmall, 
     "spec_vit_base": SpecViTBase,
     "spec_vit_large": SpecViTLarge,
     "spec_vit_huge": SpecViTHuge,
     "spec_vit_giant": SpecViTGiant,
     "resnet50": ModifiedResNet50, 
+    "vit_small": ViTSmall,
+    "vit_tiny": ViTTiny,
 }
